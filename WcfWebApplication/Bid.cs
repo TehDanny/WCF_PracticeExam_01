0d44e344d3e9b9fc94960b1827ef7eb726243f76
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-01-15 20:41:18 +0100

    Added Bid and Product classes

---
+++ WcfWebApplication/Bid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfWebApplication
+{
+    public class Bid
+    {
+        public int ItemNumber { get; set; }
+        public decimal Price { get; set; }
+        public string CustomName { get; set; }
+        public int CustomPhone { get; set; }
+    }
+}
---
+++ WcfWebApplication/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfWebApplication
+{
+    public class Product
+    {
+        public int ItemNumber { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal RatingPrice { get; set; }
+        public decimal BidPrice { get; set; }
+        public string BidCustomName { get; set; }
+        public int BidCustomPhone { get; set; }
+        public DateTime BidTimestamp { get; set; }
+    }
+}
